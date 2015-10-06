b21207007ff119aa58fce8c2735c571908088df2
Brandon <brandonskerritt51@gmail.com>
2015-10-06 09:42:06 +0100

    Commented code

--- PyCat.py
+++ PyCat.py
@@ -1,13 +1,21 @@
+# A python implementation of the CAT Tool used in Linux
+# This can be massively improved+
+
 import sys
 import shutil
 
 def main():
     if len(sys.argv) > 1:
+        # if the tool is called with an argument
+        # use that argument as the file name
+        #TODO CAT should be able to be used with more than 1 argument
         File_Name = sys.argv[1]
     else:
         print("What would you like to view?")
         File_Name = input("> ")
+        # asks the user for a file to view
         if File_Name[-4] != ".txt":
+            # if file does not end with .txt , add .txt to file
             File_Name = File_Name + ".txt"
 
 
@@ -18,9 +26,12 @@ def main():
 
 def read_File(File_Name):
     f = open(File_Name, 'r')
+    # opens and recurseively prints every line of file
     for i in f:
         print(i)
+    f.close()
 
 def close():
     input("Any key to exit!")
+    # waits for user to press any key to exit
     sys.exit()
