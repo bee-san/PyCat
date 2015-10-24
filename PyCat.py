42bc1d6845136d508de83bd69bf3be41f621405c
brandonskerritt51 <brandonskerritt51@gmail.com>
2015-10-24 22:09:44 +0100

    revamped everything

--- PyCat.py
+++ PyCat.py
@@ -1,37 +1,70 @@
 # A python implementation of the CAT Tool used in Linux
 # This can be massively improved+
 
+
 import sys
-import shutil
+from optparse import OptionParser
+
+DEVMODE = True
+
+import logging
+# imports the loggign module, creates a logging file called "ProgramLog.txt"
+logging.basicConfig(filename='_ProgramLog.txt', level=logging.DEBUG,
+					format=' %(asctime)s - %(levelname)s- %(message)s')
+
+if DEVMODE == False:
+	logging.disable(logging.CRITICAL)
+
+class CatCommand:
+    logging.debug("at class cat command")
+    def __init__(self):
+        self.count = 1
+        logging.debug("self.count set up")
+
+    def run(self, i, options):
+        e = ""
+        logging.debug("running")
+        print(i)
+        for line in i:
+            if options.showend:
+                line = line.rstrip()
+                logging.debug("show end")
+            if options.shownum:
+                logging.debug("show num")
+                line = ("{0} {1}".format(self.count, line))
+                e = "$\n"
+            logging.debug("looping")
+            self.count += 1
+            print(line, end=e)
+        logging.debug("completed for loop")
 
 def main():
-    if len(sys.argv) > 1:
-        # if the tool is called with an argument
-        # use that argument as the file name
-        #TODO CAT should be able to be used with more than 1 argument
-        File_Name = sys.argv[1]
+    usage = "usage: %prog [OPTION]... [FILE]..."
+    logging.debug("useage set up")
+    parser = OptionParser(usage=usage)
+    logging.debug("parser set up")
+    parser.add_option("-E", dest="showend", action="store_true", help="show $ at line endings")
+    logging.debug("show end set up")
+    parser.add_option("-n", dest="shownum", action="store_true", help="show line numbers")
+    logging.debug("show num set up")
+    (options, args) = parser.parse_args()
+    logging.debug(options)
+    logging.debug("options set up")
+
+
+    c = CatCommand()
+    logging.debug("created class")
+    if len(args) > 1:
+        logging.debug("called from terminal")
+        for a in args:
+            f = open(a, 'r')
+            c.run(f, options)
+            logging.debug("in for loop")
     else:
-        print("What would you like to view?")
-        File_Name = input("> ")
-        # asks the user for a file to view
-        if File_Name[-4] != ".txt":
-            # if file does not end with .txt , add .txt to file
-            File_Name = File_Name + ".txt"
+        logging.debug("else statement")
+        logging.debug(options)
+        logging.debug(sys.stdin)
+        c.run(sys.stdin, options)
 
-
-    read_File(File_Name)
-
-    close()
-
-
-def read_File(File_Name):
-    f = open(File_Name, 'r')
-    # opens and recurseively prints every line of file
-    for i in f:
-        print(i)
-    f.close()
-
-def close():
-    input("Any key to exit!")
-    # waits for user to press any key to exit
-    sys.exit()
+if __name__ == "__main__":
+    main()
